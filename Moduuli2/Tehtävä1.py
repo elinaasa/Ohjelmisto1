4e9e6b8ae8db1af56f69f33ffdb27355ec500a0c
Elina Sankila <sankilaelina@gmail.com>
2022-08-23 23:10:43 +0300

    Moduuli2

---
+++ Moduuli2/Tehtävä1.py
@@ -0,0 +1,5 @@
+
+# Kirjoita ohjelma, joka kysyy nimesi ja sen jälkeen tervehtii sinua omalla nimelläsi.
+
+kayttaja = input('Anna nimesi: ')
+print("Hauska tavata, " + kayttaja + "!")
---
+++ Moduuli2/Tehtävä2.py
@@ -0,0 +1,11 @@
+import math
+
+#Kirjoita ohjelma, joka kysyy ympyrän säteen
+# ja tulostaa sen pinta-alan.
+
+sade_str = input("Syötä ympyrän säde metreinä: ")
+sade = float(sade_str)
+pinta_ala = math.pi * (sade ** 2)
+print(f"Ympyrän pinta-ala on: {pinta_ala:1.2f} neliömetriä.")
+
+
