d07f1aba4e9d98e64fed0f7bd4d8207dc3dc63db
Omistaja <sankilaelina@gmail.com>
2022-08-23 14:34:45 +0300

    testi

--- Hello.py
+++ Hello.py
@@ -1,5 +1,16 @@
-print("Hello")
+# print("Hello")
 
-kayttaja = input('Anna nimesi: ')
-print("Hauska tavata, " + kayttaja + "!")
+# kayttaja = input('Anna nimesi: ')
+# print("Hauska tavata, " + kayttaja + "!")
+
+#Kirjoita ohjelma, joka kysyy ympyrän säteen
+# ja tulostaa sen pinta-alan.
+
+ympyra_str = input("Syötä ympyrän säde: ")
+ympyra = float(ympyra_str)
+sade = ympyra * math.pi ** 2
+print(f"Ympyrän pinta-ala on: {sade:1.2f}")
+
+
+
 
