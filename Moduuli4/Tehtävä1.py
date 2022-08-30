ddb0842ca8c277e6a568490ec06e3bece033340a
Omistaja <sankilaelina@gmail.com>
2022-08-30 15:19:01 +0300

    m4t5

---
+++ Moduuli4/Tehtävä1.py
@@ -0,0 +1,6 @@
+# Kirjoita while-toistorakennetta käyttävä ohjelma,
+# joka tulostaa kolmella jaolliset
+# luvut väliltä 1..1000.
+luku = 1
+while 1 < luku < 1000 and luku % 3 == 0:
+    print(luku)
