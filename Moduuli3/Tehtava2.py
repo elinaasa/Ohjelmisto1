b60fde4a5c9fc2db27f3259b02c38d17b5e53cdd
Omistaja <sankilaelina@gmail.com>
2022-08-30 13:42:08 +0300

    teht 3 tehty

--- Moduuli3/Tehtava2.py
+++ Moduuli3/Tehtava2.py
@@ -2,4 +2,14 @@
 # hyttiluokan (LUX, A, B, C) ja tulostaa sen sanallisen
 # kuvauksen alla olevan luettelon mukaisesti.
 # Tehtävässä on käytettävä if/elif/else-toistorakennetta.
-hyttiluokka =
+hyttiluokka = (input("Anna hyttiluokka: "))
+if hyttiluokka.upper() == "lux":
+    print("LUX on parvekkeellinen hytti yläkannella.")
+elif hyttiluokka.upper() == "A":
+    print("A on ikkunallinen hytti autokannen yläpuolella.")
+elif hyttiluokka.upper() == "B":
+    print("B on ikkunaton hytti autokannen yläpuolella.")
+elif hyttiluokka.upper() == "C":
+    print("C on ikkunaton hytti autokannen alapuolella.")
+else:
+    print("Virheellinen hyttiluokka.")
--- Moduuli3/Tehtava3.py
+++ Moduuli3/Tehtava3.py
@@ -2,3 +2,28 @@
 # biologisen sukupuolen ja hemoglobiiniarvon (g/l).
 # Ohjelma ilmoittaa, onko hemoglobiiniarvo alhainen,
 # normaali vai korkea.
+try:
+    sukupuoli = input("Anna sukupuolesi: ")
+    hemoglobiini = float(input("Anna hemoglobiiniarvosi: "))
+except:
+    print("Syöte on virheellinen.")
+
+hemoglobiini = 0
+
+# naiset
+if sukupuoli.lower() == 'nainen':
+    if hemoglobiini < 117:
+        print("Hemoglobiiniarvosi on alhainen.")
+    elif 117 <= hemoglobiini <= 175:
+        print("Hemoglobiiniarvosi on normaali.")
+    elif hemoglobiini > 175:
+        print("Hemoglobiiniarvosi on korkea.")
+
+# miehet
+if sukupuoli.lower() == "mies":
+    if hemoglobiini < 134:
+        print("Hemoglobiiniarvosi on alhainen.")
+    elif 134 <= hemoglobiini <= 195:
+        print("Hemoglobiiniarvosi on normaali.")
+    elif hemoglobiini > 195:
+        print("Hemoglobiiniarvosi on korkea.")
