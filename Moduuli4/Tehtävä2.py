4182630fed5bed0e9d34d7d3657404a56b95c4cd
Elina Sankila <sankilaelina@gmail.com>
2022-08-31 13:08:37 +0300

    mod4t3 valmis

--- Moduuli4/Tehtävä2.py
+++ Moduuli4/Tehtävä2.py
@@ -1,9 +1,8 @@
 # Kirjoita ohjelma, joka muuntaa tuumia senttimetreiksi niin kauan
 # kunnes käyttäjä antaa negatiivisen tuumamäärän.
 # Sen jälkeen ohjelma lopettaa toimintansa.
-sentit = 1
 tuumat = float(input("Syötä tuumat: "))
 while tuumat > 0:
     sentit = tuumat * 2.54
     print(f"Syöttämäsi tuumat senttimetreinä: {sentit:.2f}")
-    sentit = float(input("Syötä tuumat: "))
+    tuumat = float(input("Syötä tuumat: "))
