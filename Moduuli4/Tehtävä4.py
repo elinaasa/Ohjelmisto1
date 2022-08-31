dcb86d35d85764957d5496537644e99d42317967
Elina Sankila <sankilaelina@gmail.com>
2022-08-31 13:57:23 +0300

    mod4t4 valmis

--- Moduuli4/Tehtävä4.py
+++ Moduuli4/Tehtävä4.py
@@ -3,3 +3,17 @@
 # Kunkin arvauksen jälkeen ohjelma tulostaa tekstin:
 # Liian suuri arvaus, Liian pieni arvaus tai Oikein.
 # Huomaa, että tietokone ei saa vaihtaa lukuaan arvauskertojen välissä.
+import random
+luku = random.randint(1, 10)
+arvaus = 0
+# print(luku)
+while arvaus != luku:
+    try:
+        arvaus = float(input("Arvaa numero: "))
+        if luku > arvaus:
+            print("Liian pieni arvaus.")
+        elif arvaus > luku:
+            print("Liian suuri arvaus.")
+    except ValueError:
+        print("Virheellinen syöte.")
+print("Oikein.")
