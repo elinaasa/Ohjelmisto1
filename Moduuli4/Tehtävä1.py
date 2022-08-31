cae599406337eb7d9a0ed07f5d69716e7daaed2a
Elina Sankila <sankilaelina@gmail.com>
2022-08-31 11:55:53 +0300

    tehtävä 1 valmis

--- Moduuli4/Tehtävä1.py
+++ Moduuli4/Tehtävä1.py
@@ -1,9 +1,8 @@
 # Kirjoita while-toistorakennetta käyttävä ohjelma,
 # joka tulostaa kolmella jaolliset
 # luvut väliltä 1..1000.
-i = 1
-luku = 1
-while i == luku:
-    if 0 < luku < 1000 and luku % 3 == 0:
-        print(i)
-        i += 1
+luku = 3
+while 0 < luku < 1000:
+    if luku % 3 == 0:
+        print(luku)
+    luku += 1
