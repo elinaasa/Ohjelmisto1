4f2454ba63a44eba1f65d669cca9633fe92a98b0
Elina Sankila <sankilaelina@gmail.com>
2022-09-07 18:23:33 +0300

    mod6 t2

--- Moduuli6/Tehtävä3.py
+++ Moduuli6/Tehtävä3.py
@@ -2,3 +2,14 @@
 # ja palauttaa paluuarvonaan vastaavan litramäärän. Kirjoita pääohjelma, joka kysyy gallonamäärän
 # käyttäjältä ja muuntaa sen litroiksi. Muunnos on tehtävä aliohjelmaa hyödyntäen.
 # Muuntamista jatketaan siihen saakka, kunnes käyttäjä syöttää negatiivisen gallonamäärän.
+def gallonat_litroina(gallonat):
+    litrat = gallonat * 0.2641722
+    return litrat
+
+
+while True:
+    gallonat = float(input("Syötä bensiinin määrä gallonina: "))
+    if gallonat < 0:
+        break
+    tulos = gallonat_litroina(gallonat)
+    print("Bensiinin määrä litroina: ", tulos)
