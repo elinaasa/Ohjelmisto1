92577cdeb86972dec178ee496b5933d83b052686
Elina Sankila <sankilaelina@gmail.com>
2022-09-07 18:40:56 +0300

    mod6 t4

--- Moduuli6/Tehtävä4.py
+++ Moduuli6/Tehtävä4.py
@@ -1,4 +1,11 @@
 # Kirjoita funktio, joka saa parametrinaan listan kokonaislukuja.
 # Ohjelma palauttaa listassa olevien lukujen summan. Kirjoita testausta varten pääohjelma,
 # jossa luot listan, kutsut funktiota ja tulostat sen palauttaman summan.
-def
+lista = [1, 2, 3, 4, 5]
+
+def luvut(lista):
+    summa = sum(lista)
+    return summa
+
+
+print(luvut(lista))
