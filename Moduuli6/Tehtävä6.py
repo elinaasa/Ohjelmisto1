7c762dccde19349a8330a3950b74b9b217597620
Elina Sankila <sankilaelina@gmail.com>
2022-09-07 19:33:30 +0300

    mod6 valmis :-)

--- Moduuli6/Tehtävä6.py
+++ Moduuli6/Tehtävä6.py
@@ -3,3 +3,22 @@
 # yksikköhinnan euroina per neliömetri. Pääohjelma kysyy käyttäjältä kahden pizzan
 # halkaisijat ja hinnat sekä ilmoittaa, kumpi pizza antaa paremman vastineen rahalle
 # (eli kummalla on alhaisempi yksikköhinta). Yksikköhintojen laskennassa on hyödynnettävä kirjoitettua funktiota.
+import math
+
+
+def pizza(halkaisija, hinta):
+    pinta_ala = math.pi * (halkaisija / 2) ** 2
+    value = pinta_ala / hinta
+    return value
+
+
+hinta1 = float(input("Syötä 1. pizzan hinta: "))
+halkaisija1 = float(input("Syötä 1. pizzan halkaisija: "))
+hinta2 = float(input("Syötä 2. pizzan hinta: "))
+halkaisija2 = float(input("Syötä 2. pizzan halkaisija: "))
+value1 = pizza(halkaisija1, hinta1)
+value2 = pizza(halkaisija2, hinta2)
+if value1 < value2:
+    print("2. pizza on parempi vaihtoehto.")
+else:
+    print("1. pizza on parempi vaihtoehto.")
