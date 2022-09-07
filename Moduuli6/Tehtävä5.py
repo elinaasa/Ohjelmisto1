f2259ea141b140687a1383dfef2cea4ae648a780
Elina Sankila <sankilaelina@gmail.com>
2022-09-07 19:01:48 +0300

    mod6 t4

--- Moduuli6/Tehtävä5.py
+++ Moduuli6/Tehtävä5.py
@@ -3,3 +3,14 @@
 # parametrina saatu lista paitsi että siitä on karsittu pois kaikki parittomat luvut.
 # Kirjoita testausta varten pääohjelma, jossa luot listan,
 # kutsut funktiota ja tulostat sen jälkeen sekä alkuperäisen että karsitun listan.
+lista = [1, 2, 3, 4, 5]
+
+def parittomat(lista):
+    for n in lista:
+        if n % 2 == 0:
+            lista.remove(n)
+    return lista
+
+
+print(lista)
+print(parittomat(lista))
