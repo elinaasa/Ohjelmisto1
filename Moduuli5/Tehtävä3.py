e2c0bd569fa2c2204fbb693b6bec2d05dcf0eb07
Elina Sankila <sankilaelina@gmail.com>
2022-09-07 15:01:54 +0300

    mod4t6 valmis

--- Moduuli5/Tehtävä3.py
+++ Moduuli5/Tehtävä3.py
@@ -3,9 +3,12 @@
 # jotka ovat jaollisia vain ykkösellä ja itsellään.
 
 luku = int(input("Syötä kokonaisluku: "))
-
-for i in range(2, luku / 2 + 1):
-    if luku % 2 == 0:
-        print("Luku ei ole alkuluku.")
+if luku == 1:
+    print("Luku ei ole alkuluku.")
+else:
+    for i in range(2, luku // 2 + 1):
+        if luku % i == 0:
+            print("Luku ei ole alkuluku.")
+            break
     else:
         print("Luku on alkuluku.")
