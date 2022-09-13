970722a65048edacc07e6516cd451bffd345ebd5
Omistaja <sankilaelina@gmail.com>
2022-09-13 15:08:46 +0300

    uusia tehtäviä

---
+++ Moduuli7/Tehtävä1.py
@@ -0,0 +1,16 @@
+# Kirjoita ohjelma, joka kysyy käyttäjältä kuukauden numeron,
+# jonka jälkeen ohjelma tulostaa sitä vastaavan vuodenajan
+# (kevät, kesä, syksy, talvi). Tallenna ohjelmassasi kuukausia
+# vastaavat vuodenajat merkkijonoina monikkotietorakenteeseen.
+# Määritellään kukin vuodenaika kolmen kuukauden mittaiseksi siten,
+# että joulukuu on ensimmäinen talvikuukausi.
+talvi = ("joulukuu", "tammikuu", "helmikuu")
+"maaliskuu", "huhtikuu", "toukokuu",
+"kesäkuu", "heinäkuu", "elokuu",
+"syyskuu", "lokakuu", "marraskuu")
+
+numero = int(input("Syötä kuukauden numero: "))
+if numero <= 3:
+    print()
+
+
