8389b38a9a6cba5ddc40d380f26aced0de942f2c
Elina Sankila <sankilaelina@gmail.com>
2022-09-19 18:01:04 +0300

    t1

--- Moduuli7/Tehtävä2.py
+++ Moduuli7/Tehtävä2.py
@@ -4,7 +4,14 @@
 # Aiemmin syötetty nimi sen mukaan, syötettiinkö nimi ensimmäistä kertaa.
 # Lopuksi ohjelma luettelee syötetyt nimet yksi kerrallaan allekkain mielivaltaisessa
 # järjestyksessä. Käytä joukkotietorakennetta nimien tallentamiseen.
+nimet = set()
+
 while True:
     nimi = input("Syötä nimi: ")
     if nimi == "":
         break
+    if nimi in nimet:
+        print("Aiemmin syötetty.")
+    else:
+        print("Uusi nimi.")
+        nimet.add(nimi)
